dfbad7169be1a4a05f4cc6ea96f7fed3e3e20732
Wellington Barbosa <106507628+wellingtonbarbosadev@users.noreply.github.com>
2026-09-21 20:11:25 -0300

    feat: adicionar componentes Button e Hero com estilos e estrutura de layout

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import styles from "./Button.module.css";
+
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+  icone?: string;
+  type?: "primario" | "secundario";
+};
+
+export function Button({
+  children,
+  icone,
+  type = "secundario",
+  ...rest
+}: ButtonProps) {
+
+  const tipoBotao = type === "primario" ? styles.primario : styles.secundario
+
+  return (
+    <button className={`${styles.button} ${tipoBotao}`}>{children}</button>
+  );
+}
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,9 @@
 import AnimatedContent from "../ReactBits/AnimatedContent";
 import { DevCard } from "../DevCard/DevCard";
 
+import "./Hero.css"
+import { Button } from "../Button/Button";
+
 export function Hero() {
   const slogan =
     "Cursando Engenharia de Software na UVA. Transito entre front-end e back-end — Java, Spring Boot, Express, React — e gosto de projeto que dá trabalho de resolver.";
@@ -18,13 +21,13 @@ export function Hero() {
       threshold={0.1}
       delay={0}
     >
-      <div className="flex flex-col gap-7">
+      <div className="hero flex flex-col gap-7">
         <DevCard />
         <p className="slogan">{slogan}</p>
 
-        <section>
-          <button>ver projetos</button>
-          <button>GitHub</button>
+        <section className="buttonsHero">
+          <Button type="primario">ver projetos</Button>
+          <Button>GitHub</Button>
         </section>
       </div>
       <div className="image"></div>
