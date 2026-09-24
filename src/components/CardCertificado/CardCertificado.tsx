5aafdea764c328e392350ceac2771993d4d26b70
Wellington Barbosa <huehueb58@gmail.com>
2026-09-24 10:44:07 -0300

    feat: melhorar estilo e responsividade do componente CardCertificado

--- src/components/CardCertificado/CardCertificado.tsx
+++ src/components/CardCertificado/CardCertificado.tsx
@@ -1,14 +1,23 @@
 import type { CertificadoType } from "../../shared/data";
 import styles from "./CardCertificado.module.css";
 
-export function CardCertificado({ imagem, nome, detalhes, link }: CertificadoType) {
+export function CardCertificado({
+  imagem,
+  nome,
+  detalhes,
+  link,
+}: CertificadoType) {
   return (
     <a href={link} target="_blank" className={`${styles.cardCertificado} card`}>
       <img src={imagem} alt="" />
 
       <section>
         <h4>{nome}</h4>
-        <p title={detalhes}>{detalhes.length > 100 ? detalhes.slice(0, 100 - 3) + "..." : detalhes}</p>
+        <p title={detalhes}>
+          {detalhes.length > 100
+            ? detalhes.slice(0, 100 - 3) + "..."
+            : detalhes}
+        </p>
       </section>
     </a>
   );
